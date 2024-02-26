4e6b5928222b23a6aa1e02fd251d954a53bdc91e
rdininno <robertdininno@gmail.com>
2024-02-26 12:45:58 -0800

    update main page
    this updates the main page to have a the logo appear
    then simulate a scroll that triggeres a disolve
    effect on the logo. once the logo opacity is 0 the
    content below it will be come interactive.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MenuItem from "./components/MenuItem.js";
 import About from "./components/About.js";
 import Design from "./components/Design.js";
@@ -7,6 +7,38 @@ import Design from "./components/Design.js";
 function App() {
   const [activeComponent, setActiveComponent] = useState("");
   const [activeDesignContent, setActiveDesignContent] = useState("");
+  const [opacity, setOpacity] = useState(1);
+  const [, setFakeScrollY] = useState(0);
+  const maxScroll = 300;
+
+  useEffect(() => {
+    const handleWheel = (e) => {
+      if (opacity > 0) {
+        e.preventDefault();
+        const deltaY = e.deltaY;
+        setFakeScrollY((prev) => {
+          const newScrollY = Math.max(0, prev + deltaY);
+          const newOpacity = 1 - newScrollY / maxScroll;
+          setOpacity(Math.max(0, newOpacity));
+          return newScrollY;
+        });
+      }
+    };
+
+    window.addEventListener("wheel", handleWheel, { passive: false });
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [opacity]);
+
+  useEffect(() => {
+    if (opacity <= 0) {
+      document.body.style.overflow = "visible";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  }, [opacity]);
 
   const handleDesignClick = () => {
     setActiveComponent("Design");
@@ -15,11 +47,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className="header">
-        <img src="./images/Freak_Heat_Waves.jpg" alt="header" />
+      <div
+        className="header main-page-image"
+        style={{
+          opacity,
+          pointerEvents: opacity === 0 ? "none" : "auto",
+        }}
+      >
+        <img src="./images/Di_Ninno_LOGO.PNG" alt="header" />
       </div>
 
-      <div className="main-page">
+      <div className="main-page-content">
         <div className="menu">
           <MenuItem name="Design" color="#2ebe36" onClick={handleDesignClick} />
           <MenuItem
@@ -36,7 +74,7 @@ function App() {
               setActiveContent={setActiveDesignContent}
               activeContent={activeDesignContent}
             />
-          )}{" "}
+          )}
         </div>
       </div>
     </div>
